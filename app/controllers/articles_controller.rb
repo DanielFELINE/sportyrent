5aabd10b15a3d5383c1552d20fd9e9e17c356d35
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-05-27 16:53:27 +0200

    ajout devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,11 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
 end
