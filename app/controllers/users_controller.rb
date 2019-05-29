8a2b917c72ce045c69b2ec5266bb9d8b7e4fc385
stephane VINCENT <stephane.vincent0@gmail.com>
2019-05-29 11:59:20 +0200

    db address + sauvegarde prenom, nom, addresse, photo du user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
   def create
     raise
   end
+  
+  def article_params
+    params.require(:user).permit(:first_name, :last_name, :picture, :address, :phone_number)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_134610) do
+ActiveRecord::Schema.define(version: 2019_05_29_093854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_134610) do
     t.string "last_name"
     t.string "phone_number"
     t.string "picture"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
