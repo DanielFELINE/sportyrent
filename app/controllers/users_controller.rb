f67d4505d94f3ef5f010191830a425883733a867
stephane VINCENT <stephane.vincent0@gmail.com>
2019-05-30 15:08:40 +0200

    init dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :reservations, :myprecious]
 
   def create
     raise
   end
+
+  def show
+  end
+
+  def reservations
+  end
+
+  def myprecious
+  end
+
+  private
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def article_params
     params.require(:user).permit(:first_name, :last_name, :picture, :address, :phone_number)
   end
