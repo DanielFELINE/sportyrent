534380cf4dce88c7808785c2467944308f436877
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-05-30 12:50:07 +0200

    map

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,11 @@ class Article < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
   validates :sport, inclusion: { in: SPORTS }
+  geocoded_by :address 
+  after_validation :geocode
+  
+  def address
+    self.user.address
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+Article.destroy_all
 
+5.times do |article|
+  Article.create!(
+    name: "ballon",
+    description: "parfait",
+    sport: ["Football", "Tennis", "Volleyball"].sample,
+    photo: "https://images.unsplash.com/photo-1521731978332-9e9e714bdd20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80",
+  )
+end
