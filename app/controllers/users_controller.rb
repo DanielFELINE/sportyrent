e38bb667181d59b9ebea7ac29309529565807547
Lecaut Laury <laury.lecaut@gmail.com>
2019-05-30 16:35:18 +0200

    dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,5 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :reservations, :myprecious]
-
-  def create
-    raise
-  end
-
   def show
-  end
-
-  def reservations
-  end
-
-  def myprecious
-  end
-
-  private
-  
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def article_params
-    params.require(:user).permit(:first_name, :last_name, :picture, :address, :phone_number)
+    @user = current_user
   end
 end
