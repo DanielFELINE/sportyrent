14d08523b595775078ef6f388af14c58ac3ec87c
stephane VINCENT <stephane.vincent0@gmail.com>
2019-05-30 14:27:35 +0200

    ajout dispo article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,12 @@
 class Article < ApplicationRecord
   belongs_to :user
+  after_initialize :default_values
   mount_uploader :photo, PhotoUploader
   validates :sport, inclusion: { in: SPORTS }
+
+  private
+
+  def default_values
+    self.dispo ||= false
+  end
 end
