5b7b23f0661d6ec408f992b871b6377ee172690f
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-05-31 16:49:38 +0200

    pb map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,5 @@ import "../plugins/flatpickr";
 import "../plugins/date_picking";
 import { calculatePrice } from "../plugins/date_picking";
 calculatePrice();
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 initMapbox();
